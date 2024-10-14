a479848c75fc9a6fcde72ae7b3d507bcc4686370
JeremyPerezGit <jeremy.perez13@hotmail.fr>
2024-10-14 08:55:31 +0200

    [FEAT] added secret feature!

--- index.js
+++ index.js
@@ -5,6 +5,66 @@ let cookieMultiplieur = 1;
 
 // FONCTIONS
 
+// KONAMI CODE
+// a key map of allowed keys
+var allowedKeys = {
+  37: "left",
+  38: "up",
+  39: "right",
+  40: "down",
+  65: "a",
+  66: "b",
+};
+
+// the 'official' Konami Code sequence
+var konamiCode = [
+  "up",
+  "up",
+  "down",
+  "down",
+  "left",
+  "right",
+  "left",
+  "right",
+  "b",
+  "a",
+];
+
+// a variable to remember the 'position' the user has reached so far.
+var konamiCodePosition = 0;
+
+// add keydown event listener
+document.addEventListener("keydown", function (e) {
+  // get the value of the key code from the key map
+  var key = allowedKeys[e.keyCode];
+  // get the value of the required key from the konami code
+  var requiredKey = konamiCode[konamiCodePosition];
+
+  // compare the key with the required key
+  if (key == requiredKey) {
+    // move to the next key in the konami code sequence
+    konamiCodePosition++;
+
+    // if the last key is reached, activate cheats
+    if (konamiCodePosition == konamiCode.length) {
+      activateCheats();
+      konamiCodePosition = 0;
+    }
+  } else {
+    konamiCodePosition = 0;
+  }
+});
+
+function activateCheats() {
+  const img = document.querySelector(".clickableImg");
+  img.src = "./assets/img/click/citrouille.png";
+
+  var audio = new Audio("./assets/sound/konami_intro.mp3");
+  audio.play();
+
+  alert("Bien joué, digne de Parzival! 🥚");
+}
+
 // ouvrir modale
 function openModal() {
   document.querySelector(".overlay").style.display = "block";
@@ -23,7 +83,6 @@ buttonClick.addEventListener("click", () => {
   clickCookie += 1 * cookieMultiplieur;
   document.getElementById("nombreDeCookiesBanque").innerHTML = clickCookie;
 });
-localStorage.setItem("nbCookies", clickCookie);
 
 // Mateusz
 
