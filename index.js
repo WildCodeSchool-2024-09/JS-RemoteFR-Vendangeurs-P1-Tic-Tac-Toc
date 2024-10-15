f7f6caac4aadd14ff2f6ce932d9fce436780bea2
JeremyPerezGit <jeremy.perez13@hotmail.fr>
2024-10-15 17:12:58 +0200

    Few changes

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ let cookieMultiplieur = 1;
 // FONCTIONS
 
 // KONAMI CODE
-// a key map of allowed keys
-var allowedKeys = {
+
+const allowedKeys = {
   37: "left",
   38: "up",
   39: "right",
@@ -16,8 +16,7 @@ var allowedKeys = {
   66: "b",
 };
 
-// the 'official' Konami Code sequence
-var konamiCode = [
+const konamiCode = [
   "up",
   "up",
   "down",
@@ -30,22 +29,14 @@ var konamiCode = [
   "a",
 ];
 
-// a variable to remember the 'position' the user has reached so far.
-var konamiCodePosition = 0;
+let konamiCodePosition = 0;
 
-// add keydown event listener
 document.addEventListener("keydown", function (e) {
-  // get the value of the key code from the key map
-  var key = allowedKeys[e.keyCode];
-  // get the value of the required key from the konami code
-  var requiredKey = konamiCode[konamiCodePosition];
+  let key = allowedKeys[e.keyCode];
+  let requiredKey = konamiCode[konamiCodePosition];
 
-  // compare the key with the required key
   if (key == requiredKey) {
-    // move to the next key in the konami code sequence
     konamiCodePosition++;
-
-    // if the last key is reached, activate cheats
     if (konamiCodePosition == konamiCode.length) {
       activateCheats();
       konamiCodePosition = 0;
@@ -57,10 +48,16 @@ document.addEventListener("keydown", function (e) {
 
 function activateCheats() {
   const img = document.querySelector(".clickableImg");
-  img.src = "./assets/img/click/citrouille.png";
+  img.src = "./assets/img/click/Mangekyou.png";
 
-  var audio = new Audio("./assets/sound/konami_intro.mp3");
+  let audio = new Audio("./assets/sound/itachi.mp3");
   audio.play();
+
+  const title = document.querySelector("#title");
+  title.textContent = "Itachi Clicker";
+
+  const mangekyou = document.querySelector("#typeName");
+  mangekyou.textContent = "Sharingans";
 
   alert("Bien joué, digne de Parzival! 🥚");
 }
