1b9d17a571d1de3269bbe8896fddae5fe950ddd7
AURELIA GUARINO <aureliaguarino0@gmail.com>
2024-10-17 16:12:35 +0200

    [STYLE]glitter

--- darkmode.js
+++ darkmode.js
@@ -1,10 +1,13 @@
 const themeSwitch = document.getElementById("theme-switch");
 const header = document.querySelector("header");
 const main = document.querySelector("main");
+const icon_cursor = document.querySelector("icon_cursor");
+
 
 themeSwitch.addEventListener("click", () => {
   header.classList.toggle("darkmode");
   main.classList.toggle("darkmode");
+  icon_cursor.classList("darkmode");
 });
 
 // TEST LOCALSAVE
